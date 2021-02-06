53bae5cbd2f67f5a8ce675bf5569ed0b80528930
João Victor <joaovitormunizlopes@gmail.com>
2021-02-06 15:57:22 -0300

    All tests working

---
+++ src/useCases/UpdateUser/UpdateUserUseCase.spec.ts
@@ -0,0 +1,57 @@
+import AppError from '../../errors/AppError';
+
+import { FakeUsersRepository } from '../../repositories/fakeImplementations/FakeUsersRepository';
+import { UpdateUserUseCase } from './UpdateUserUseCase';
+import { User } from '../../entities/User';
+
+let updateUserUseCase:UpdateUserUseCase;
+let fakeUsersRepository:FakeUsersRepository;
+
+describe('Updating user', ()=>{
+
+    beforeEach(() => {
+
+        fakeUsersRepository = new  FakeUsersRepository()
+        updateUserUseCase = new  UpdateUserUseCase(fakeUsersRepository);
+
+    })
+
+    it('Should not be able to update',async()=>{
+
+        let user = new User({
+            name:"João Vitor",
+            email:"joaovitormunizlopes@gmail.com",
+            userPassword:"mensalao63"
+        })
+
+        expect(updateUserUseCase.execute(user)).rejects.toBeInstanceOf(AppError)
+
+    })
+
+
+    it('Should be able to update',async()=>{
+
+
+        const user = new User({
+            name:"João Vitor",
+            email:"joaovitormunizlopes@gmail.com",
+            userPassword:"mensalao63"
+        })
+
+        fakeUsersRepository.save(user);
+
+        const updatedUser = await updateUserUseCase.execute({
+            id:user.id,
+            name:"Joao 2",
+            userPassword:"adasdasdasd",
+            email:"joao@gmail.com"
+        })
+
+        expect(updatedUser.name).toBe('Joao 2');
+        expect(updatedUser.email).toBe('joao@gmail.com');
+        expect(updatedUser.userPassword).toBe('adasdasdasd');
+
+    })
+
+
+})
